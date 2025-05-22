3b12d65a9a98f4ad608a899d3a0f95b5bf1a9fb7
Vicky Farinango <vickyfarinango@gmail.com>
2025-05-22 17:15:54 -0400

    feat: Implements a one to many relationship between the Goal model and the Task model

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,9 +1,13 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .task import Task
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
+    task_ids: Mapped[list["Task"]] = relationship(back_populates="goal")
     
     def to_dict(self):
         return {
@@ -15,5 +19,6 @@ class Goal(db.Model):
     @classmethod
     def from_dict(cls, goal_data):
         return cls(
-            title=goal_data["title"]
+            title=goal_data["title"],
+            task_ids=goal_data.get("task_id", [])
         )
--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from datetime import datetime
+from typing import Optional
 from ..db import db
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .goal import Goal
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] =  mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     
     def to_dict(self):
@@ -26,6 +33,7 @@ class Task(db.Model):
     @classmethod
     def from_dict(cls, task_data):
         return cls(
+            goal_id=task_data.get("goal_id"),
             title=task_data["title"],
             description=task_data["description"],
             completed_at=task_data["completed_at"]
